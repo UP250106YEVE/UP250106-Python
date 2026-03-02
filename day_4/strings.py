90475b8120d6296d544d7f398b26515f11cda75c
HP <up250106@alumnos.upa.edu.mx>
2026-03-02 11:30:31 -0600

    avance dia 4

---
+++ day_4/strings.py
@@ -0,0 +1,9 @@
+# DAY 4
+
+# Exercises day 4
+
+# 1. Concatenate the string 'Coding', 'For', 'All'. Single string.
+
+strings = ['Thirty', 'Days', 'Of', 'Python']
+frase = ' '.join(strings)
+print(frase)
