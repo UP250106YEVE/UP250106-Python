5f2229468c31c6144d76a8a4625970f8e6cf8ec6
HP <up250106@alumnos.upa.edu.mx>
2026-03-05 11:25:55 -0600

    same code

--- day_4/strings.py
+++ day_4/strings.py
@@ -7,3 +7,25 @@
 strings = ['Thirty', 'Days', 'Of', 'Python']
 frase = ' '.join(strings)
 print(frase)
+
+# 2. Concatenate the string 'Coding', 'For', 'All' to a single string
+
+strings = ['Coding', 'For', 'All']
+frase = ' '.join(strings)
+print(frase)
+
+# 3. Declare a variable named company and assign it to an initial value "Coding For All".
+
+company = 'Coding for all'
+
+# 4. Print the variable company using print().
+
+print(company)
+
+# 5. Print the length of the company string using len() method and print().
+print(len(company))
+
+# 6. Change all the characters to uppercase letters using upper() method.
+
+company = 'Coding For All'
+print(company.upper())
