54feba4e43a3d2c4e41c71d9cef164c141c6fabb
HP <up250106@alumnos.upa.edu.mx>
2026-03-12 21:37:34 -0600

    avance 3

--- day_5/lists.py
+++ day_5/lists.py
@@ -22,3 +22,38 @@ print('Middle company: ', it_companies[3])
 print('Last company: ', it_companies[6])
 
 it_companies_copy = it_companies.copy()
+it_companies_copy.remove('Google')
+print('Modified list: ', it_companies_copy)
+
+it_companies.append('IT')
+print('Adding \'IT\' to the list: ', it_companies)
+it_companies.insert(3, 'IT')
+print('Putting \'IT\' in the middle of the list: ', it_companies)
+
+it_companies_copy_2 = it_companies.copy()
+it_companies_copy_3 = it_companies.copy()
+
+it_companies_copy[1] = 'GOOGLE'
+print('One company in uppercase: ', it_companies_copy)
+str = '#; '
+it_companies_copy.extend(str)
+print('List with a \'#; \' string: ', it_companies_copy)
+does_exist = 'Oracle' in it_companies
+print('Does \'Oracle\' exists in the list? ', does_exist)
+it_companies.sort()
+print('Sort list: ', it_companies)
+it_companies.sort(reverse=True)
+print('Reversed list: ', it_companies)
+slice_out = it_companies_copy_2[0:3]
+print('Slicing out the first 3 companies: ', slice_out)
+slice_out_2 = it_companies_copy_2[6:9]
+print('Slicing out the last 3 companies: ', slice_out_2)
+
+del it_companies_copy_2[3]
+print('Removing first \'IT\' which is also the middle one: ', it_companies_copy_2)
+
+del it_companies_copy_2[7]
+print('Removing last \'IT\': ', it_companies_copy_2)
+
+it_companies_copy_3.clear()
+print('Deleting the \'IT\' companies list: ', it_companies_copy_3)
