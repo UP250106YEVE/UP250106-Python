79d8d8f852ef10f0acce8f3a721eda588aaba52d
HP <up250106@alumnos.upa.edu.mx>
2026-03-12 21:36:12 -0600

    avance 1

---
+++ day_5/lists.py
@@ -0,0 +1,24 @@
+#Day_5
+#Excercises level 1
+lst = []
+print('Empty list: ',lst)
+brands = ['Porsche', 'Volkswagen', 'Audi', 'BMW', 'Volvo', 'Ford']
+print('My list: ',brands)
+print('Lenght of my list: ', len(brands))
+first = brands[0]
+print('The first item is: ',first)
+middle = brands[2]
+print('The middle item is: ',middle)
+last = brands[5]
+print('The last item is: ',last)
+
+mixed_data_types = ['Santos', 18, 1.76, 'Engaged', 'Jalisco']
+
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print('New list: ', it_companies)
+print('Number of companies: ', len(it_companies))
+print('First company: ', it_companies[0])
+print('Middle company: ', it_companies[3])
+print('Last company: ', it_companies[6])
+
+it_companies_copy = it_companies.copy()
