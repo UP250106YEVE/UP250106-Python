dd7230b3e6d7b748101b359831642f740360337e
HP <up250106@alumnos.upa.edu.mx>
2026-03-19 12:16:14 -0600

    day 8 avances

---
+++ day_8/dictionaries.py
@@ -0,0 +1,30 @@
+#EXERCISES
+dog = {}
+
+dog = {'name':'Mandarino',
+    'color':'Yellow',
+    'breed':'si',
+    'legs':'orange',
+    'age':'at least 1 day'}
+print(dog)
+
+student = {'first_name':'Yex',
+        'last_name':'vela',
+        'gender':'Male',
+        'age':'18',
+        'marital_status':'Con mi mujer',
+        'skills':['Unknown'],
+        'country':'Mexico',
+        'City':'Aguascalientes',
+        'adress':'Fco. I. Madero. #238, Calvillo'
+        }
+print(student)
+
+print(len(student))
+
+print(student['skills'])
+
+student['skills'] = 'habilidad1'
+
+print(student)
+
