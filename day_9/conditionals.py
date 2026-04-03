669e569e054aeb9fbb23a39a9df3e22a61ef2b71
HP <up250106@alumnos.upa.edu.mx>
2026-04-02 20:19:04 -0600

    ejercicio 4

--- day_9/conditionals.py
+++ day_9/conditionals.py
@@ -16,3 +16,14 @@ elif a < b:
     print(f"{b} es mayor que {a}")
 else:
     print(f"{a} y {b} son iguales")
+
+
+age_a = int(input('Introduce tu edad: '))
+age_b = int(input('Introduce la edad de tu amigo: '))
+
+if age_a > age_b:
+    print('Eres mayor que tu amigo')
+elif age_a < age_b:
+    print('Tu amigo es mayor que tú')
+else:
+    print('Tienen la misma edad')
