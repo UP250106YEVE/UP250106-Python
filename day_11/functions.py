3ce6e61a7783ea8714c1a9e707f0c9af6b472914
HP <up250106@alumnos.upa.edu.mx>
2026-04-06 10:38:16 -0600

    2

--- day_11/functions.py
+++ day_11/functions.py
@@ -38,3 +38,58 @@ print('Check season: ', check_season('November'))
 
 def calculate_slope (x1, y1, x2, y2):
     if x2 - x1 == 0:
+        return 'Slope is undefined (vertical line)'
+    slope = (y2 - y1)/(x2-x1)
+    return slope
+print('Calculate slope: ', calculate_slope(1, 2, 3, 4))
+
+def solve_quadratic_eqn (a, b, c):
+    discriminant = b**2 - 4*a*c
+    if a == 0:
+        return "Not a quadratic equation (a cannot be 0)"
+    import math
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return {x1, x2}
+    elif discriminant == 0:
+        x = -b / (2*a)
+        return {x}
+    else:
+        real_part = -b / (2*a)
+        imaginary_part = math.sqrt(abs(discriminant)) / (2*a)
+        root1 = complex(real_part, imaginary_part)
+        root2 = complex(real_part, -imaginary_part)
+        return {root1, root2}
+print('Solve quadratic equation: ', solve_quadratic_eqn(1, -5, 6))
+
+def print_list (my_list):
+    for element in my_list:
+        print('List element: ', element)
+car_brands = ['Volkswagen', 'Porsche', 'Audi', 'BMW']
+print_list(car_brands)
+
+def reverse_list (array):
+    reverse_array = []
+    for i in range(len(array)-1, -1, -1):
+        reverse_array.append(array[i])
+    return reverse_array
+print('Reversed list: ', reverse_list([1, 2, 3, 4, 5]))
+print('Reversed list: ', reverse_list(['A', 'B', 'C']))
+
+def capitalize_list_items(my_list):
+    capitalized_list = []
+    for item in my_list:
+        capitalized_list.append(str(item).capitalize())
+    return capitalized_list
+objects = ['paper', 'wallet' , 'bottle', 'backpack']
+print('Capitalized llist: ', capitalize_list_items(objects))
+
+def add_item(my_list, item):
+    new_list = my_list.copy()
+    new_list.append(item)
+    return new_list
+food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print('Adding item to list: ', add_item(food_stuff, 'Meat'))
+numbers = [2, 3, 7, 9]
+print('Adding item to list: ', add_item(numbers, 5))
